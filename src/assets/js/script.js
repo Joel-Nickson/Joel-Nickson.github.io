c4c628e27dfddcbecca3465cf1568174376a3c83
Joel-Nickson <joel2000nickson@gmail.com>
2022-07-02 19:12:31 +0530

    update gmap location

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -110,24 +110,3 @@ for (let i = 0; i < filterBtn.length; i++) {
     });
 
 }
-
-
-
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
-
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-    formInputs[i].addEventListener("input", function () {
-
-        // check form validation
-        if (form.checkValidity()) {
-            formBtn.removeAttribute("disabled");
-        } else {
-            formBtn.setAttribute("disabled", "");
-        }
-
-    });
-}
--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -16,22 +16,18 @@
     )
       emailval = true;
     else emailval = false;
-
-    console.log(emailval, email);
   }
 
   function validatesubject(e) {
     subject = e.target.value;
     if (subject) subjectval = true;
     else subjectval = false;
-    console.log(subjectval, subject);
   }
 
   function validatetextarea(e) {
     message = e.target.value;
     if (message) messageval = true;
     else messageval = false;
-    console.log(messageval, message);
   }
 </script>
 
@@ -43,7 +39,7 @@
   <figure>
     <iframe
       title="google map"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9%3A0x921f6aaa74197fdb!2sSacramento%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7858.362621228839!2d76.28024049999999!3d10.00187745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d642b68be2b%3A0x41d7dbaef6cc2b87!2sPachalam%2C%20Kochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1656769025191!5m2!1sen!2sin"
       width="400"
       height="300"
       loading="lazy"
