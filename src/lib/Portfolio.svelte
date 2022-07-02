27dee1de98ed0308d9ada366e3141f528d7f41a8
Joel-Nickson <joel2000nickson@gmail.com>
2022-07-02 22:37:59 +0530

    sveltified portfolio

--- src/lib/Portfolio.svelte
+++ src/lib/Portfolio.svelte
@@ -1,269 +1,52 @@
+<script>
+  import { portfolioContent, portfolioCategory } from "./stores";
+  let currCategory = "all";
+  let portfolio_category = portfolioCategory;
+  let portfolio_content = portfolioContent;
+  const selectCategory = (e) =>
+    (currCategory = e.target.innerText.toLowerCase());
+</script>
+
 <header>
   <h2 class="h2 article-title">Portfolio</h2>
 </header>
 
 <section class="projects">
   <ul class="filter-list">
-    <li class="filter-item">
-      <button class="active" data-filter-btn>All</button>
-    </li>
-
-    <li class="filter-item">
-      <button data-filter-btn>Web design</button>
-    </li>
-
-    <li class="filter-item">
-      <button data-filter-btn>Applications</button>
-    </li>
-
-    <li class="filter-item">
-      <button data-filter-btn>Web development</button>
-    </li>
+    {#each portfolio_category as item}
+      <li class="filter-item">
+        <button
+          on:click={selectCategory}
+          class={currCategory === item ? "active" : ""}
+          data-filter-btn>{item}</button
+        >
+      </li>
+    {/each}
   </ul>
-
-  <div class="filter-select-box">
-    <button class="filter-select" data-select>
-      <div class="select-value" data-selecct-value>Select category</div>
-
-      <div class="select-icon">
-        <ion-icon name="chevron-down" />
-      </div>
-    </button>
-
-    <ul class="select-list">
-      <li class="select-item">
-        <button data-select-item>All</button>
-      </li>
-
-      <li class="select-item">
-        <button data-select-item>Web design</button>
-      </li>
-
-      <li class="select-item">
-        <button data-select-item>Applications</button>
-      </li>
-
-      <li class="select-item">
-        <button data-select-item>Web development</button>
-      </li>
-    </ul>
-  </div>
-
   <ul class="project-list">
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web development"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
+    {#each portfolio_content as item}
+      <li
+        class="project-item  {item.datacategory === currCategory ||
+        currCategory === 'all'
+          ? 'active'
+          : ''}"
+        data-filter-item
+        data-category={item.datacategory}
+      >
+        <a href={item.target !== "" ? item.target : "#"}>
+          <figure class="project-img">
+            <div class="project-item-icon-box">
+              <ion-icon name="eye-outline" />
+            </div>
 
-          <img
-            src="./src/assets/images/project-1.jpg"
-            alt="finance"
-            loading="lazy"
-          />
-        </figure>
+            <img src={item.src} alt={item.alt} loading="lazy" />
+          </figure>
 
-        <h3 class="project-title">Finance</h3>
+          <h3 class="project-title">{item.alt}</h3>
 
-        <p class="project-category">Web development</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web development"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-2.png"
-            alt="orizon"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Orizon</h3>
-
-        <p class="project-category">Web development</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web design"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-3.jpg"
-            alt="fundo"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Fundo</h3>
-
-        <p class="project-category">Web design</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="applications"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-4.png"
-            alt="brawlhalla"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Brawlhalla</h3>
-
-        <p class="project-category">Applications</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web design"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-5.png"
-            alt="dsm."
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">DSM.</h3>
-
-        <p class="project-category">Web design</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web design"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-6.png"
-            alt="metaspark"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">MetaSpark</h3>
-
-        <p class="project-category">Web design</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web development"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-7.png"
-            alt="summary"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Summary</h3>
-
-        <p class="project-category">Web development</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="applications"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-8.jpg"
-            alt="task manager"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Task Manager</h3>
-
-        <p class="project-category">Applications</p>
-      </a>
-    </li>
-
-    <li
-      class="project-item  active"
-      data-filter-item
-      data-category="web development"
-    >
-      <a href="#">
-        <figure class="project-img">
-          <div class="project-item-icon-box">
-            <ion-icon name="eye-outline" />
-          </div>
-
-          <img
-            src="./src/assets/images/project-9.png"
-            alt="arrival"
-            loading="lazy"
-          />
-        </figure>
-
-        <h3 class="project-title">Arrival</h3>
-
-        <p class="project-category">Web development</p>
-      </a>
-    </li>
+          <p class="project-category">{item.datacategory}</p>
+        </a>
+      </li>
+    {/each}
   </ul>
 </section>
--- src/lib/stores.js
+++ src/lib/stores.js
@@ -8,6 +8,69 @@ export const isActiveData = writable({
     contact: "deactive",
 });
 
+export const portfolioCategory = [
+    "all",
+    "web design",
+    "application",
+    "web development",
+];
+export const portfolioContent = [
+    {
+        datacategory: "web development",
+        src: "./src/assets/images/project-1.jpg",
+        alt: "finance",
+        target: "",
+    },
+    {
+        datacategory: "web development",
+        src: "./src/assets/images/project-2.png",
+        alt: "orizon",
+        target: "",
+    },
+    {
+        datacategory: "web design",
+        src: "./src/assets/images/project-3.jpg",
+        alt: "fundo",
+        target: "",
+    },
+    {
+        datacategory: "application",
+        src: "./src/assets/images/project-4.png",
+        alt: "brawlhalla",
+        target: "",
+    },
+    {
+        datacategory: "web design",
+        src: "./src/assets/images/project-5.png",
+        alt: "dsm.",
+        target: "",
+    },
+    {
+        datacategory: "web design",
+        src: "./src/assets/images/project-6.png",
+        alt: "metaspark",
+        target: "",
+    },
+    {
+        datacategory: "web development",
+        src: "./src/assets/images/project-7.png",
+        alt: "summary",
+        target: "",
+    },
+    {
+        datacategory: "application",
+        src: "./src/assets/images/project-8.jpg",
+        alt: "task manager",
+        target: "",
+    },
+    {
+        datacategory: "web development",
+        src: "./src/assets/images/project-9.png",
+        alt: "arrival",
+        target: "",
+    },
+];
+
 export const profile = writable({
     'name': 'Joel Nickson',
     'email': 'notjoeln@gmail.com',
