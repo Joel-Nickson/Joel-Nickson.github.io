a0c95d532d853b66d358cdbf8aefc1d58eb3ce29
Joel-Nickson <joel2000nickson@gmail.com>
2022-07-02 14:19:36 +0530

    svelte code made from https://github.com/codewithsadee/vcard-personal-portfolio/

---
+++ src/lib/About.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Clients from "./Clients.svelte";
+  import Services from "./Services.svelte";
+  import TestimonialModal from "./TestimonialModal.svelte";
+  import Testimonials from "./Testimonials.svelte";
+</script>
+
+<article class="about  active" data-page="about">
+  <header>
+    <h2 class="h2 article-title">About me</h2>
+  </header>
+
+  <section class="about-text">
+    <p>
+      I'm Creative Director and UI/UX Designer from Sydney, Australia, working
+      in web development and print media. I enjoy turning complex problems into
+      simple, beautiful and intuitive designs.
+    </p>
+
+    <p>
+      My job is to build your website so that it is functional and user-friendly
+      but at the same time attractive. Moreover, I add personal touch to your
+      product and make sure that is eye-catching and easy to use. My aim is to
+      bring across your message and identity in the most creative way. I created
+      web design for many famous brand companies.
+    </p>
+  </section>
+
+  <Services />
+  <Testimonials />
+  <TestimonialModal />
+  <Clients />
+</article>
---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,101 @@
+<aside class="sidebar" data-sidebar>
+  <div class="sidebar-info">
+    <figure class="avatar-box">
+      <img
+        src="./src/assets/images/my-avatar.png"
+        alt="Richard hanrick"
+        width="80"
+      />
+    </figure>
+
+    <div class="info-content">
+      <h1 class="name" title="Richard hanrick">Richard hanrick</h1>
+
+      <p class="title">Web developer</p>
+    </div>
+
+    <button class="info_more-btn" data-sidebar-btn>
+      <span>Show Contacts</span>
+
+      <ion-icon name="chevron-down" />
+    </button>
+  </div>
+
+  <div class="sidebar-info_more">
+    <div class="separator" />
+
+    <ul class="contacts-list">
+      <li class="contact-item">
+        <div class="icon-box">
+          <ion-icon name="mail-outline" />
+        </div>
+
+        <div class="contact-info">
+          <p class="contact-title">Email</p>
+
+          <a href="mailto:richard@example.com" class="contact-link"
+            >richard@example.com</a
+          >
+        </div>
+      </li>
+
+      <li class="contact-item">
+        <div class="icon-box">
+          <ion-icon name="phone-portrait-outline" />
+        </div>
+
+        <div class="contact-info">
+          <p class="contact-title">Phone</p>
+
+          <a href="tel:+12133522795" class="contact-link">+1 (213) 352-2795</a>
+        </div>
+      </li>
+
+      <li class="contact-item">
+        <div class="icon-box">
+          <ion-icon name="calendar-outline" />
+        </div>
+
+        <div class="contact-info">
+          <p class="contact-title">Birthday</p>
+
+          <time datetime="1982-06-23">June 23, 1982</time>
+        </div>
+      </li>
+
+      <li class="contact-item">
+        <div class="icon-box">
+          <ion-icon name="location-outline" />
+        </div>
+
+        <div class="contact-info">
+          <p class="contact-title">Location</p>
+
+          <address>Sacramento, California, USA</address>
+        </div>
+      </li>
+    </ul>
+
+    <div class="separator" />
+
+    <ul class="social-list">
+      <li class="social-item">
+        <a href="#" class="social-link">
+          <ion-icon name="logo-facebook" />
+        </a>
+      </li>
+
+      <li class="social-item">
+        <a href="#" class="social-link">
+          <ion-icon name="logo-twitter" />
+        </a>
+      </li>
+
+      <li class="social-item">
+        <a href="#" class="social-link">
+          <ion-icon name="logo-instagram" />
+        </a>
+      </li>
+    </ul>
+  </div>
+</aside>
