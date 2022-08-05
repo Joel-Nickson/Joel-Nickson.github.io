ce4171eef2a3b834d5fbe101960b6b3891f45a4e
Joel Nickson <joel2000nickson@gmail.com>
2022-08-05 20:01:01 +0530

    update about

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,8 +1,9 @@
 <script>
-  import Clients from "./Clients.svelte";
-  import Services from "./Services.svelte";
-  import TestimonialModal from "./TestimonialModal.svelte";
-  import Testimonials from "./Testimonials.svelte";
+  // import Clients from "./Clients.svelte";
+  // import Services from "./Services.svelte";
+  // import TestimonialModal from "./TestimonialModal.svelte";
+  // import Testimonials from "./Testimonials.svelte";
+  import Contact from "./Contact.svelte";
 </script>
 
 <header>
@@ -11,20 +12,22 @@
 
 <section class="about-text">
   <p>
-    I'm Junior Frontend Developer from Kerala, India, pursuing a bachelor's
-    degree in Computer Science and Engineering. I enjoy turning complex problems
-    into simple, beautiful and intuitive designs.
+    I'm a passionate amateur student who loves building projects, sharing
+    knowledge and contributing to the Open Source Community. I enjoy the art of
+    learning and then using it in my day to day life.
   </p>
 
   <p>
-    My job is to build your website so that it is functional and user-friendly
-    but at the same time attractive. Moreover, I add personal touch to your
-    product and make sure that is eye-catching and easy to use. My aim is to
-    bring across your message and identity in the most creative way.
+    My skills are most proficient in Developing & Deploying Apps and Websites
+    with Flutter, React, Node & Flask , but I consider working with these as a
+    side hassel. I am always looking to improve my skills and learn new things.
+    And even though I am not very strong at it, I would love to work on ML
+    related projects more. But more than that my interests are in building.
+    Something that would benefit everyone equally.
   </p>
 </section>
-
-<Services />
+<Contact />
+<!-- <Services /> -->
 <!-- <Testimonials />
 <TestimonialModal />
 <Clients /> -->
