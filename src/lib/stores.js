e4c553ac8a12f5dc9586dda8d27f6b135cff9b2e
Joel Nickson <joel2000nickson@gmail.com>
2022-08-05 23:07:12 +0530

    updated store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -10,48 +10,66 @@ export const isActiveData = writable({
 
 export const portfolio_category = [
     "all",
-    "web developement",
-    "application",
-    "artificial intelligence",
+    "web",
+    "app",
+    "ai",
     "other",
     "coming soon",
 ];
 export const portfolio_content = [
     {
-        datacategory: "search engine",
-        src: "images/project-1.jpg",
-        alt: "meilisearch",
-        target: "https://www.meilisearch.com/",
-    }, {
-        datacategory: "web developement",
-        src: "images/project-2.png",
+        datacategory: "web",
+        src: "images/default-project-dark.jpg",
         alt: "KEAM college predictor",
-        target: "https://github.com/FISAT-PROJECTS/keam-project",
-    }, {
-        datacategory: "web developement",
-        src: "images/project-3.jpg",
-        alt: "telegom",
-        target: "https://github.com/Co-Science/tele-go-m",
-    }, {
-        datacategory: "application",
-        src: "images/project-4.png",
+        target: "https://github.com/FISAT-projectS/keam-project",
+    },
+    {
+        datacategory: "search engine",
+        src: "images/default-project-dark.jpg",
+        alt: "meilisearch",
+        target: "https://github.com/meilisearch/meilisearch-go/pull/224",
+    },
+    {
+        datacategory: "app",
+        src: "images/default-project-dark.jpg",
         alt: "libre-fit",
         target: "https://github.com/HackOnTheWay",
-    }, {
-        datacategory: "artificial intelligence",
-        src: "images/project-7.png",
+    },
+    {
+        datacategory: "app",
+        src: "images/default-project-dark.jpg",
+        alt: "Maze",
+        target: "https://github.com/Joel-Nickson/Maze-in-java",
+    },
+    {
+        datacategory: "ai",
+        src: "images/default-project-dark.jpg",
         alt: "Video Summarizer",
         target: "",
-    }, {
-        datacategory: "application",
-        src: "images/project-6.png",
-        alt: "Maze",
-        target: "",
-    }, {
-        datacategory: "coming soon",
-        src: "images/project-9.png",
-        alt: "dragon a.i.",
-        target: "",
+    },
+    {
+        datacategory: "web",
+        src: "images/default-project-dark.jpg",
+        alt: "telegom",
+        target: "https://github.com/Co-Science/tele-go-m",
+    },
+    {
+        datacategory: "web",
+        src: "images/default-project-dark.jpg",
+        alt: "Hackfit Website",
+        target: "https://github.com/ICEFOSS/hackfit",
+    },
+    {
+        datacategory: "web",
+        src: "images/default-project-dark.jpg",
+        alt: "ICEFOSS Website",
+        target: "https://github.com/ICEFOSS/icefoss",
+    },
+    {
+        datacategory: "web",
+        src: "images/default-project-dark.jpg",
+        alt: "MechFisat Website",
+        target: "https://github.com/Joel-Nickson/aegis-2022-vite",
     },
 ];
 
@@ -91,7 +109,7 @@ export const testimonials = [
 
 export const profile = {
     'name': 'Joel Nickson',
-    'email': 'notjoeln@gmail.com',
+    'email': 'connectwithjoelnickson@gmail.com',
     'mobile': '(+91) XXX-XXX-XXXX',
     'role': 'Web Developer',
     'linkedin': 'https://www.linkedin.com/in/joel-nickson-0a27721a5/',
@@ -99,6 +117,21 @@ export const profile = {
     'twitter': 'https://twitter.com/JoelNickson5',
     'location': 'Kochi, Kerala, India',
 }
+
+
+export const sideBarInfo = {
+    profile: [
+        { icon: "mail-outline", name: "Email", value: profile.email },
+        { icon: "location-outline", name: "Location", value: profile.location }
+    ],
+    social: [
+        { icon: "logo-linkedin", value: profile.linkedin },
+        { icon: "logo-github", value: profile.github },
+        // { icon: "logo-twitter", value: profile.twitter }
+    ]
+}
+// ,{icon:"phone-portrait-outline",name:"Mobile",value:profile.mobile}
+// ,{icon:"calendar-outline",name:"Birthday",value:"Sep 29,2000"}
 
 export const education = [
     {
@@ -178,18 +211,6 @@ export const services = [{
 
 export const projects = [
     {
-        'company': 'Meilisearch',
-        'title': 'Paris based Open Source Search Engine Startup',
-        'location': 'Paris, Ile-de-France, France',
-        'role': 'Open Source Contributor',
-        'type': ['Open Source', 'Backend'],
-        'start': '10/21',
-        'end': '11/21',
-        'language': ['Go'],
-        'description': 'Added functionality to return search results based on index for the Golang backend',
-        'url': 'https://www.meilisearch.com/',
-    },
-    {
         'company': 'FISAT',
         'title': 'KEAM college predictor',
         'location': 'Mookkannoor, Angamaly, Kerala',
@@ -203,17 +224,16 @@ export const projects = [
         'url': 'https://github.com/FISAT-PROJECTS/keam-project',
     },
     {
-        'company': 'Co Science',
-        'title': 'Telegom',
-        'location': 'Kochi, Kerala',
-        'role': 'Backend Developer',
+        'company': 'Meilisearch',
+        'title': 'Paris based Open Source Search Engine Startup',
+        'location': 'Paris, Ile-de-France, France',
+        'role': 'Open Source Contributor',
         'type': ['Open Source', 'Backend'],
-        'start': '05/21',
-        'end': '12/21',
+        'start': '10/21',
+        'end': '11/21',
         'language': ['Go'],
-        'framework': [],
-        'description': 'Minimalistic Telegram Bot backend made with Gorilla Web toolkit',
-        'url': 'https://github.com/Co-Science/tele-go-m',
+        'description': 'Added functionality to return search results based on index for the Golang backend',
+        'url': 'https://github.com/meilisearch/meilisearch-go/pull/224',
     },
     {
         'company': '',
@@ -226,6 +246,19 @@ export const projects = [
         'language': ['Java'],
         'framework': [],
         'description': 'GUI based Path finding algorithm using BFS & DFS',
+        'url': 'https://github.com/Joel-Nickson/Maze-in-java',
+    },
+    {
+        'company': 'Co Science',
+        'title': 'Telegom',
+        'location': 'Kochi, Kerala',
+        'role': 'Backend Developer',
+        'type': ['Open Source', 'Backend'],
+        'start': '05/21',
+        'end': '12/21',
+        'language': ['Go'],
+        'framework': [],
+        'description': 'Minimalistic Telegram Bot backend made with Gorilla Web toolkit',
         'url': 'https://github.com/Co-Science/tele-go-m',
     },
 ]
@@ -255,7 +288,7 @@ export const projectsComingSoon = [
     },
     {
         'company': 'Microsoft',
-        'title': 'Research Intern',
+        'title': 'Intern',
         'type': ['Web', 'Frontend'],
         'start': '7/21',
         'end': '',
